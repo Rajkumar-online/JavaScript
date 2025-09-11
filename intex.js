9c0fe4905725cce940b942a55143623c1af7759c
Rajkumar-online <rajkumarmk04@gmail.com>
2025-09-11 14:50:09 +0530

    hi raj

---
+++ intex.js
@@ -0,0 +1,10 @@
+// Concatenate
+var name = "Sundaramoorthy";
+var secound = " S";
+var output = name + secound;
+
+console.log(output); // Sundaramoorthy S
+
+// Template String
+var user = `${name} hey welcome to my site`;
+console.log(user); // Sundaramoorthy hey welcome to my site
